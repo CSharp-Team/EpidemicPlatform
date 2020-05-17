42ca13437b1d86b4dc32e4b2335706ccc922af97
yue <xiaobaiwhu@gmail.com>
2020-05-17 21:02:25 +0800

    增加按钮，提示，地图功能
    Co-Authored-By: Clement <yueclementine@users.noreply.github.com>

--- src/components/common/form.vue
+++ src/components/common/form.vue
@@ -4,27 +4,72 @@
     <el-input v-model="formLabelAlign.name"></el-input>
   </el-form-item>
   <el-form-item label="物品数量">
-    <el-input v-model="formLabelAlign.region"></el-input>
+    <el-input v-model="formLabelAlign.number"></el-input>
   </el-form-item>
   <el-form-item label="联系方式">
-    <el-input v-model="formLabelAlign.type"></el-input>
+    <el-input v-model="formLabelAlign.tel"></el-input>
   </el-form-item>
+ <el-form-item label="省市 :" prop="description">
+                             <el-cascader size="large" clearable class="customized_input_340" change-on-select :options="provinceOptions" v-model="selectedOptions" @change="handleChange">
+                             </el-cascader>
+                         </el-form-item>
+   <el-form-item>
+      <el-button type="primary" @click="onSubmit">立即提交</el-button>
+    </el-form-item>
 </el-form>
 </template>
 
 <script>
+
+import
+{
+    provinceAndCityData,
+    regionData,
+    provinceAndCityDataPlus,
+    regionDataPlus,
+    CodeToText,
+    TextToCode
+}
+from 'element-china-area-data';
     export default {
+
        data() {
          return {
+          provinceOptions: provinceAndCityData,
+                     selectedOptions: [],
            labelPosition: 'right',
            formLabelAlign: {
-             name: ' ',
+             name: '',
              number: '',
              tel: ''
            }
          };
-       }
+       },
+       methods: {
+
+        handleChange(val) {
+                   console.log(val)
+                   this.ruleForm.province = CodeToText[val[0]]
+                   this.ruleForm.city = CodeToText[val[1]]
+                   let sameProvince = provinceAndCityData.find((province)=>{
+                                           return province.label == data.province
+                                       })
+                                       let sameCity = sameProvince.children.find((city)=>{
+                                           return city.label == data.city
+                                       })
+                                       this.selectedOptions = [sameProvince.value,sameCity.value]
+               },
+             onSubmit() {
+               console.log('submit!');
+                this.$notify({
+                         title: '恭喜您提交成功',
+                         message: '',
+                         type: 'success'
+             })
+           }
      }
+    }
+
 </script>
 
 <style scoped>
