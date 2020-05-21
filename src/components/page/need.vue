37dc4fdc107227d26499d5a033540bdd7aa4ca75
Aezqsss <823439378@qq.com>
2020-05-21 19:54:19 +0800

    修正need和give的导航栏

--- src/components/page/need.vue
+++ src/components/page/need.vue
@@ -1,16 +1,12 @@
 <template>
-<el-container>
-  <el-header>
-    <div class="nav">
-      <v-nav></v-nav>
+    <div>
+
+      <vNav></vNav>
+      <vForm></vForm>
+
+      
     </div>
-  </el-header>
-  <el-main>
-    <div class="form">
-      <v-form></v-form>
-    </div>
-  </el-main>
-</el-container>
+
 </template>
 <script>
  import vForm from '../common/form'
