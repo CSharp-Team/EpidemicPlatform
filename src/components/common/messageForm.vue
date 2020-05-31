2f971579065661b0e7cc060301f6c46d5d90289f
yue <xiaobaiwhu@gmail.com>
2020-05-31 12:25:43 +0800

    更新消息界面

---
+++ src/components/common/messageForm.vue
@@ -0,0 +1,94 @@
+<template>
+    <el-tabs :tab-position="tabPosition" style="height: 900px;" v-model="activeName" @tab-click="handleClick" stretch=true>
+    <el-tab-pane label="已发送信息" name="已发送信息">
+        <el-table :data="sentMessage" style="width: 100%">  
+                  <el-table-column label="信息编号" prop="messageId"></el-table-column>
+                  <el-table-column label="接收人" prop="recipient"></el-table-column>                
+                  <el-table-column label="申请物品" prop="items.itemname"></el-table-column>
+                  <el-table-column label="申请数量" prop="items.itemCount"></el-table-column>
+                  <el-table-column label="接收状态" prop="messageState"></el-table-column>
+        </el-table>
+    </el-tab-pane>
+    <el-tab-pane label="未接收信息">
+        <el-table :data="receiveMessageUnHandled" style="width: 100%">  
+                  <el-table-column label="信息编号" prop="messageId"></el-table-column>
+                  <el-table-column label="发送人" prop="applicant"></el-table-column>                
+                  <el-table-column label="申请物品" prop="items.itemName"></el-table-column>
+                  <el-table-column label="申请数量" prop="items.itemCount"></el-table-column>
+                  <el-table-column label="是否接收">
+                      <template slot-scope="props">
+                      <el-button @click="ReciClick(props.row)">接收</el-button>
+                      </template>
+                  </el-table-column>
+        </el-table>
+    </el-tab-pane>
+    <el-tab-pane label="已接收信息">
+        <el-table :data="receiveMessageHandled" style="width: 100%">  
+                  <el-table-column label="信息编号" prop="messageId"></el-table-column>
+                  <el-table-column label="发送人" prop="applicant"></el-table-column>                
+                  <el-table-column label="申请物品" prop="items.itemName"></el-table-column>
+                  <el-table-column label="申请数量" prop="items.itemCount"></el-table-column>
+                  <el-table-column label="申请时间" prop="time"></el-table-column>
+         </el-table>
+    </el-tab-pane>
+  </el-tabs>
+</template>>
+<script>
+    import axios from "axios"
+    import request from "../../utils/request"
+    var self=this
+ export default {    
+    data() {
+      return {
+        sentMessage:[],
+        receiveMessageHandled:[],
+        receiveMessageUnHandled:[],
+        activeName: '已发送信息',
+        tabPosition:'top'
+      };
+    },
+    methods: {
+      handleClick(tab, event) {
+        console.log(tab, event);
+      },
+      ReciClick(Id){
+          var url = '/g/confirm?MessageId='+Id
+          get(url).then(response=>{
+              console.log(response)
+          })
+      },
+      getdata(){
+          var user = ' ';
+          user = this.$store.state.user;
+          var senturl = '/g/getSentMessage?name='+user;
+          get(senturl)
+          .then(reponse=>{
+              console.log(response)
+              sekf.sentMessage=reponse.data
+          }).catch(e=>this.$message.console.error(e.response.data))
+          
+          var reciHandledurl = '/g/getReceiveMessageHandled?name='+user;
+          get(reciHandledurl)
+          .then(reponse=>{
+              console.log(response)
+              sekf.receiveMessageHandled=reponse.data
+          }).catch(e=>this.$message.console.error(e.response.data))
+
+          var unreciHandledurl = '/g/getReceiveMessageUnHandled?name='+user;
+          get(unreciHandledurl)
+          .then(reponse=>{
+              console.log(response)
+              sekf.receiveMessageUnHandled=reponse.data
+          }).catch(e=>this.$message.console.error(e.response.data))
+
+      }
+    },
+    mounted(){
+        this.getdata();
+        self = this ;
+    }
+  };
+</script>>
+<style>
+
+</style>>
--- src/components/page/message.vue
+++ src/components/page/message.vue
@@ -3,14 +3,20 @@
     <topnav></topnav>
 
 
+  <el-main>
+
+      <messageForm></messageForm>
+
+  </el-main>
   </div>
 </template>
 
 <script>
 import topnav from '@/components/common/nav'
+import messageForm from '@/components/common/messageForm'
 export default {
   components:{
-    topnav
+    topnav,messageForm
   }
 
 }
@@ -18,5 +24,10 @@ export default {
 
 
 <style scoped>
-
+.el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 50px;
+  }
 </style>
