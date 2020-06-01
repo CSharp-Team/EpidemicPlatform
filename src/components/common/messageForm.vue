9df6199bd414bc14c95acb7d821f9e1dd927e7de
yue <xiaobaiwhu@gmail.com>
2020-06-02 01:58:39 +0800

    消息界面实现

--- src/components/common/messageForm.vue
+++ src/components/common/messageForm.vue
@@ -1,57 +1,95 @@
 <template>
 
-    <el-tabs :tab-position="tabPosition" style="height: 900px;" v-model="activeName" @tab-click="handleClick" stretch=true>
-    <el-tab-pane label="已发送消息" name="已发送信息">
-        <el-table :data="sentMessage" style="width: 100%">  
-                  <el-table-column label="信息编号" prop="messageId"></el-table-column>
-                  <el-table-column label="接收人" prop="recipient"></el-table-column>  
-                  <el-table-column label="时间" prop="time" ></el-table-column>             
-                      <el-table-column type="expand">
-                    <template slot-scope="props">
-                      <el-table :data="props.row.items">
-                        <el-table-column label="物品名称" prop="itemName"></el-table-column>
-                        <el-table-column label="物品数量" prop="itemCount"></el-table-column>
-                      </el-table>
-                    </template>
-                  </el-table-column>
-                  <el-table-column label="接收状态" prop="meaasgeState"></el-table-column>
-        </el-table>
-    </el-tab-pane>
-    <el-tab-pane label="未接收消息">
-        <el-table :data="receiveMessageUnHandled" style="width: 100%">  
-                  <el-table-column label="信息编号" prop="messageId"></el-table-column>
+  <el-tabs :tab-position="tabPosition2" style="height: 900px;" v-model="activeName" @tab-click="handleClick" >
+    <el-tab-pane label="供给订单消息" name="供给订单消息">
+      <el-tabs :tab-position="tabPosition" style="height: 800px;" v-model="activeName2" @tab-click="handleClick" >
+         <el-tab-pane label="我收到的申请" name="我收到的申请">
+             <el-table :data="FirstMessage" style="width: 100%">  
+                  <el-table-column label="消息编号" prop="messageId"></el-table-column>
                   <el-table-column label="发送人" prop="applicant"></el-table-column>                
                   <el-table-column label="时间" prop="time" ></el-table-column>             
                       <el-table-column type="expand">
                     <template slot-scope="props">
-                      <el-table :data="props.row.items">
-                        <el-table-column label="物品名称" prop="itemName"></el-table-column>
-                        <el-table-column label="物品数量" prop="itemCount"></el-table-column>
+                      <el-table :data="props.row.items" :show-header="false">
+                        <el-table-column  prop="itemName"></el-table-column>
+                        <el-table-column  prop="itemCount"></el-table-column>
+                      </el-table>
+                    </template>
+                       </el-table-column>
+                  <el-table-column label="是否接受">
+                        <template slot-scope="scope">
+             <el-button v-if="scope.row.meaasgeState=='Y'" type="primary" size="medium"  :disabled=true>已发货</el-button>
+             <el-button v-else-if="scope.row.meaasgeState=='N'"  type="primary" size="medium"  @click="FirstClick(scope.row)">确认发货</el-button>
+                         </template>
+                      <!-- <template slot-scope="props">
+                      <el-button :disabled="!props.row.meaasgeState==='N'" @click="ReciClick(props.row)">接收申请</el-button>
+                      </template> -->
+                  </el-table-column>
+             </el-table>
+         </el-tab-pane>
+         <el-tab-pane label="我接受的申请" name="我接受的申请">
+             <el-table :data="ThirdMessage" style="width: 100%">  
+                  <el-table-column label="消息编号" prop="messageId"></el-table-column>
+                  <el-table-column label="发送人" prop="applicant"></el-table-column>                
+                  <el-table-column label="时间" prop="time" ></el-table-column>             
+                      <el-table-column type="expand">
+                    <template slot-scope="props">
+                      <el-table :data="props.row.items" :show-header="false">
+                        <el-table-column prop="itemName"></el-table-column>
+                        <el-table-column prop="itemCount"></el-table-column>
                       </el-table>
                     </template>
                   </el-table-column>
                   <el-table-column label="是否接收">
                       <template slot-scope="props">
-                      <el-button @click="ReciClick(props.row)">接收</el-button>
+                         <el-button v-if="props.row.meaasgeState=='Y'" type="primary" size="medium"  :disabled=true>供需完成</el-button>
+                         <el-button v-else-if="props.row.meaasgeState=='N'"  type="primary" size="medium"  @click="ConfirmClick(scope.row)">确认对方已收到</el-button>
                       </template>
                   </el-table-column>
-        </el-table>
+               </el-table>
+         </el-tab-pane>
+      </el-tabs>
     </el-tab-pane>
-    <el-tab-pane label="已接收消息">
-        <el-table :data="receiveMessageHandled" style="width: 100%">  
-                  <el-table-column label="信息编号" prop="messageId"></el-table-column>
-                  <el-table-column label="发送人" prop="applicant"></el-table-column>                
+    <el-tab-pane label="需求订单消息" name="需求订单信息">
+       <el-tabs :tab-position="tabPosition" style="height: 800px;" v-model="activeName3" @tab-click="handleClick" stretch=true>
+         <el-tab-pane label="我发出的申请" name="我发出的申请">
+             <el-table :data="sentMessage" style="width: 100%">  
+                  <el-table-column label="消息编号" prop="messageId"></el-table-column>
+                  <el-table-column label="接收人" prop="recipient"></el-table-column>  
                   <el-table-column label="时间" prop="time" ></el-table-column>             
-                 <el-table-column type="expand">
+                      <el-table-column type="expand">
                     <template slot-scope="props">
-                      <el-table :data="props.row.items">
-                        <el-table-column label="物品名称" prop="itemName"></el-table-column>
-                        <el-table-column label="物品数量" prop="itemCount"></el-table-column>
+                      <el-table :data="props.row.items" :show-header="false">
+                        <el-table-column prop="itemName"></el-table-column>
+                        <el-table-column prop="itemCount"></el-table-column>
                       </el-table>
                     </template>
                   </el-table-column>
-                  <el-table-column label="申请时间" prop="time"></el-table-column>
+                  <el-table-column label="接收状态" prop="meaasgeState"></el-table-column>
+              </el-table>
+         </el-tab-pane>
+           <el-tab-pane label="待收货的物资" name="待收货的物资">
+             <el-table :data="SecondMessage" style="width: 100%">  
+                  <el-table-column label="消息编号" prop="messageId"></el-table-column>
+                  <el-table-column label="发送人" prop="applicant"></el-table-column>                
+                  <el-table-column label="时间" prop="time" ></el-table-column>             
+                      <el-table-column type="expand">
+                    <template slot-scope="props">
+                      <el-table :data="props.row.items" :show-header="false">
+                        <el-table-column label="" prop="itemName"></el-table-column>
+                        <el-table-column lable="" prop="itemCount"></el-table-column>
+                      </el-table>
+                    </template>
+                  </el-table-column>
+                  <el-table-column label="是否接收">
+                     <template slot-scope="scope">
+             <el-button v-if="scope.row.meaasgeState=='Y'" type="primary" size="medium"  :disabled=true>已收货</el-button>
+             <el-button v-else-if="scope.row.meaasgeState=='N'"  type="primary" size="medium"  @click="SecondClick(scope.row)">确认收货</el-button>
+                         </template>
+                  </el-table-column>
          </el-table>
+       </el-tab-pane>
+     </el-tabs>
     </el-tab-pane>
   </el-tabs>
 
@@ -64,18 +102,55 @@ var self=this
       return {
         sentMessage:[],
         receiveMessageHandled:[],
+        FirstMessage:[],
+        SecondMessage:[],
+        ThirdMessage:[],
         receiveMessageUnHandled:[],
-        activeName: '已发送信息',
-        tabPosition:'top'
+        activeName: '供给订单消息',
+        activeName2:'我收到的申请',
+        activeName3:'我发出的申请',
+        tabPosition:'left',
+        tabPosition2:'top'
       };
     },
     methods: {
       handleClick(tab, event) {
         console.log(tab, event);
       },
-      ReciClick(Id){
-          var url = '/g/confirm?MessageId='+Id
-          axios.get(url).then(response=>{
+      ConfirmClick(obj){
+        obj.meaasgeState="Y"
+        console.log(obj)
+        var url = '/g/Message/confirm?MessageId='+obj.messageId
+      },
+      SecondClick(obj){
+          obj.meaasgeState="Y"
+          console.log(obj.meaasgeState)
+          var url = '/g/Message/sendThirdRequest'
+          axios.post(url,{
+            "Applicant":obj.applicant,
+          	"Recipient":obj.recipient,
+          	"SupplyId":obj.supplyId,
+	          "NeedId":obj.needId,
+	          "Time":"2020.05.23",
+	          "TargetMessageId":obj.messageId,
+	          "Items":obj.items
+          }).then(response=>{
+              console.log(response)
+          })
+      },
+      FirstClick(obj){
+          obj.meaasgeState="Y"
+          console.log(obj.meaasgeState)
+          var url = '/g/Message/sendSecondRequest'
+          axios.post(url,{
+            "Applicant":obj.applicant,
+          	"Recipient":obj.recipient,
+          	"SupplyId":obj.supplyId,
+	          "NeedId":obj.needId,
+	          "Time":"2020.05.23",
+	          "TargetMessageId":obj.messageId,
+	          "Items":obj.items
+          }).then(response=>{
               console.log(response)
           })
       },
@@ -117,13 +192,27 @@ var self=this
               console.log(response)
               for(var i=0;i<response.data.length;i++)
               {
-                  self.receiveMessageUnHandled.push(response.data[i]);
+                if(response.data[i].messageType=="First")
+                  {self.FirstMessage.push(response.data[i]);}
+                  else if(response.data[i].messageType=="Second"){
+                    self.SecondMessage.push(response.data[i])
+                  }
+                  else if(response.data[i].messageType=="Third"){
+                    self.ThirdMessage.push(response.data[i])
+                  }
               }
           })
           console.log("\n\n\n")
           console.log("self.receiveMessageUnHandled")
           console.log(self.receiveMessageUnHandled)
           console.log("\n\n\n")
+                
+         console.log(self.receiveMessageUnHandled)
+         console.log(11111111111111111111111111)
+         console.log(self.FirstMessage)
+         console.log(self.SecondMessage)
+         console.log(self.ThirdMessage)
+    
       }
     },
     mounted(){
